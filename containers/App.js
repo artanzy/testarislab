947a433d93bdc49c91112089311da9d438ede242
tanAInno <natanon.p@ku.th>
2020-01-15 15:54:15 +0700

    start test

---
+++ containers/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, RefreshRoute, Switch, Link } from 'react-router-dom';
+
+class App extends Component {
+
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                </Switch>
+            </BrowserRouter>
+        )
+    }
+
+}
+
+export default App
