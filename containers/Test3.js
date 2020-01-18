7610f2249d21471389af95fa00f98506b97836ef
tanAInno <tanlnw@gmail.com>
2020-01-18 18:59:19 +0700

    Test3 success

--- containers/Test3.js
+++ containers/Test3.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import '../css/Test3.css'
+import axios from 'axios';
 
 class Test3 extends Component {
 
+    state = {
+        message : ""
+    }
+
+    async postMessage(){
+        await axios.post("http://localhost:3030",{
+            token: 'eWpuYJgPODNdYcjYAHRfMb6Ej0kb5O1kDLpkNfNzwqs',
+            message: this.state.message
+        }).catch(error => console.log(error))
+    }
+
+    handleChange(e){
+        this.setState({message : e.target.value})
+    }
+
     render() {
         return(
-            <div/>
+            <div className="container">
+                <div className="test3-header">LINE Notify:</div>
+                <textarea className="test3-input"
+                    value={this.state.message}
+                    onChange={e => this.handleChange(e)}
+                />
+                <button className="test3-button" onClick={() => this.postMessage()}>แจ้งเตือน</button>
+            </div>
         )
     }
 
---
+++ expressServer/app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var request = require('request');
+var cors = require('cors');
+// Set up the express app
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
+app.use(cors())
+// get all todos
+app.post('/', (req, res) => {
+    console.log(req.body)
+    var token = req.body.token;
+    var message = req.body.message;
+   
+    request({
+        method: 'POST',
+        uri: 'https://notify-api.line.me/api/notify',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        auth: {
+          'bearer': token
+        },
+        form: {
+          message: message
+        }
+      }, (err, httpResponse, body) => {
+        if(err){
+          console.log(err);
+        } else {
+          res.json({
+            httpResponse: httpResponse,
+            body: body
+          });
+        }
+    });
+});
+const PORT = 3030;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
